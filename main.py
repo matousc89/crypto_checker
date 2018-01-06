14354b54b4cad745fb9cbad8e4f9129d8bb9b11f
Your Name <you@example.com>
2018-01-06 13:02:58 +0100

    Added 1W. Better erro checking.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import requests
 import time
+from datetime import datetime
 
 
 
@@ -46,14 +47,16 @@ def compile_html(targets, filename):
             </style>
         </head>
         <body>
+            <h3>{}</h3>
             <nav>
                 <a href="view_alpha.html">Alphabetical</a>&nbsp;|
-                <a href="view_24.html">24 hours gain</a>&nbsp;|
-                <a href="view_48.html">48 hours gain</a>
+                <a href="view_1D.html">1 day gain</a>&nbsp;|
+                <a href="view_2D.html">2 days gain</a>&nbsp;|
+                <a href="view_1W.html">1 week gain</a>
             </nav>
             <table>
                 <tr>
-                    <th>Symbol</th><th>24 hours [%]</th><th>48 hours [%]</th><th>Status</th>
+                    <th>Symbol</th><th>1 day [%]</th><th>2 days [%]</th><th>1 week [%]</th><th>Status</th>
                 </tr>
                 {}
             </table>
@@ -88,23 +91,37 @@ def compile_html(targets, filename):
         cell_symbol = cell_template.format(symbol_style, target["symbol"])
         ch_1D = round(target["change_1D"], 2)
         ch_2D = round(target["change_2D"], 2)
+        ch_1W = round(target["change_1W"], 2)
         cell_1D = cell_template.format(html_value_style(ch_1D), ch_1D)
         cell_2D = cell_template.format(html_value_style(ch_2D), ch_2D)
+        cell_1W = cell_template.format(html_value_style(ch_1W), ch_1W)
         cell_status = cell_template.format("", target["status"])
         # create line and append it to content
-        line = cell_symbol + cell_1D + cell_2D + cell_status
+        line = cell_symbol + cell_1D + cell_2D + cell_1W + cell_status
         html_content += line_template.format(line)
-    html = html_template.format(css, html_content)
+    # get date time
+    heading = str(datetime.now())
+    # put it together and save it
+    html = html_template.format(css, heading, html_content)
     with open(filename, "w") as f:
         f.write(html)
 
 
 targets = ["ARK","QSP","DGD","GTO","ADA","GVT","BCPT","TNB","OAX","BCD","KNC","SNGLS","BAT","DNT","BQX","NULS","ELF","CTR","POE","APPC","BNB","ZEC","ZRX","VIB","LUN","RDN","WABI","NEBL","OST","MANA","WINGS","CND","KMD","VEN","LSK","BRD","ADX","ICX","SUB","CDT","POWR","DLT","ARN","EDO","TNT","PPT","WTC","ETC","WAVES","DASH","NAV","TRIG","OMG","XVG","ICN","FUN","GXS","GXS","XMR","AMB","MTL","STORJ","HSR","NEO","XRP","LRC","MTH","ENG","EOS","YOYO","IOTA","BTS","QTUM","STRAT","XZC","AION","EVX","LINK","AST","SNM","SNT","SALT","TRX","BNT","ENJ"]
+# targets = ["ETH", "GTO", "BTC"]
 currency = "USD"
 
 time_actual = time.time()
 time_1D = time_actual - 3600*24
 time_2D = time_actual - 3600*24*2
+time_1W = time_actual - 3600*24*7
+
+
+SCOPES = [
+    {"name": "1D", "timestamp": time_actual - 3600*24},
+    {"name": "2D", "timestamp": time_actual - 3600*24*2},
+    {"name": "1W", "timestamp": time_actual - 3600*24*7},
+]
 
 
 # targets = targets[:3]
@@ -114,52 +131,44 @@ items = []
 for target in targets:
     time.sleep(0.1)
     print("Downloading: ", target)
+    item = {
+        "symbol": target,
+        "price": 0,
+        "status": "Ok",
+    }
+    for scope in SCOPES:
+        item["price_"+scope["name"]] = 0
+        item["change_" + scope["name"]] = 0
+    # get price
     try:
-        # get prices
         price_now = get_price(target, currency)[target][currency]
-        price_1D = get_price(target, currency, time_1D)[target][currency]
-        price_2D = get_price(target, currency, time_2D)[target][currency]
-        # calculate changes in percents 24h, 48h
-        change_1D = (price_now / price_1D * 100) - 100
-        change_2D = (price_now / price_2D * 100) - 100
-        # store it
-        item = {
-            "symbol": target,
-            "price": price_now,
-            "price_1D": price_1D,
-            "price_2D": price_2D,
-            "change_1D": change_1D,
-            "change_2D": change_2D,
-            "status": "Ok"
-        }
-        items.append(item)
-        print("Done.")
     except Exception as e:
-        print(e)
-        # store it
-        item = {
-            "symbol": target,
-            "price": 0,
-            "price_1D": 0,
-            "price_2D": 0,
-            "change_1D": 0,
-            "change_2D": 0,
-            "status": e,
-        }
-        items.append(item)
-
-
-
+        item["status"] = "Unable to get data - " + str(e)
+    if price_now:
+        # get other prices and calculate changes
+        for scope in SCOPES:
+            try:
+                price = get_price(target, currency, scope["timestamp"])[target][currency]
+                item["price_" + scope["name"]] = price
+                change = (price_now / price * 100) - 100
+                item["change_" + scope["name"]] = change
+            except Exception as e:
+                item["status"] = e
+    # store it
+    items.append(item)
+    print(item["status"])
 
 
 compile_html(items, "view_alpha.html")
 
 items = sorted(items, key=lambda k: k['change_1D'], reverse=True)
-compile_html(items, "view_24.html")
+compile_html(items, "view_1D.html")
 
 items = sorted(items, key=lambda k: k['change_2D'], reverse=True)
-compile_html(items, "view_48.html")
+compile_html(items, "view_2D.html")
 
+items = sorted(items, key=lambda k: k['change_1W'], reverse=True)
+compile_html(items, "view_1W.html")
 
 
 
